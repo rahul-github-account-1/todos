794e889e620b741ac5da0a55e0bc3f0eb3767f99
rahul-github-account-1 <rahulk999777@gmail.com>
2024-09-10 12:17:20 +0530

    postgres local added

--- main.go
+++ main.go
@@ -16,9 +16,15 @@ func main(){
 	if err!=nil{
 		log.Fatal("Unable to Load config in main package: ",err);
 	}
+	fmt.Printf("DB User: %s\n", config.DBUser)
+fmt.Printf("DB Name: %s\n", config.DBName)
+fmt.Printf("DB Host: %s\n", config.DBHost)
+fmt.Printf("DB Port: %s\n", config.DBPort)
+fmt.Printf("JWT Key: %s\n", config.JWTKey)
 
-	// database_conn_string:=fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",config.DBUser,config.DBPassword,config.DBName,config.DBHost,config.DBPort);
-	database_conn_string:="postgresql://neondb_owner:Cj9lMErfik1d@ep-aged-field-a5yms3hz.us-east-2.aws.neon.tech/neondb?sslmode=require";
+
+	database_conn_string:=fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",config.DBUser,config.DBPassword,config.DBName,config.DBHost,config.DBPort);
+	// database_conn_string:="postgresql://neondb_owner:Cj9lMErfik1d@ep-aged-field-a5yms3hz.us-east-2.aws.neon.tech/neondb?sslmode=require";
 	
 	db,err:=gorm.Open(postgres.Open(database_conn_string),&gorm.Config{});
 	if err!=nil{
